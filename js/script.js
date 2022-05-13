ed81c7ce86d987961b2f0f62f7e59f95e15c3e54
ida <idasusannajohansson@gmail.com>
2022-05-13 15:36:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+// Skapar popup + alert
+let username = prompt("Enter your name: ");
+alert("Welcome, " + username + "!");
+
+
+// Skriver ut medelande i konsollen
+console.log("Page loaded!")
+
+function switchTheme () {
+    document.querySelector("body").classList.toggle("light-theme");
+    document.querySelectorAll(".buttons").classList.toggle("btnLight-theme");
+ 
+    /*
+    document.querySelectorAll(".buttons").classList.toggle("btnLight-theme");
+    */
+}
+
+function disableBtn () {
+    document.getElementById("switchThemeId").disabled = true;
+}
+
+/*
+Avaktiverar alla knappar utom disabled
+document.getElementsByClassName("buttons").disabled = true; 
+*/
+
+function activateBtn () {
+    document.getElementById("switchThemeId").disabled = false;
+}
